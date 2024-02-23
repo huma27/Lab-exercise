109308f4f086c7a93a0304a31b36caf09cbcb32d
huma27 <hum27@pitt.edu>
2024-02-23 11:15:18 -0500

    added user inputs

--- simple_program.py
+++ simple_program.py
@@ -1,1 +1,7 @@
-print("Welcome to Lab Exercise)
+print("Welcome to Lab Exercise")
+
+ name = input("what is your name")
+
+ age = input("How old are you?")
+
+    print("Your name is " + name "and you are " + age "years old") 
