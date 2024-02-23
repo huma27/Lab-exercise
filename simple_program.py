11d3edcb90df1fb9e8cad87464652997085b1c09
huma27 <hum27@pitt.edu>
2024-02-23 11:22:35 -0500

    fixed format

--- simple_program.py
+++ simple_program.py
@@ -1,6 +1,6 @@
 print("Welcome to Lab Exercise")
 
-name = input("what is your name")
-age = input("How old are you?")
+name = input("what is your name: ")
+age = input("How old are you? ")
 
 print("Your name is", name, "and you are", age, "years old")
